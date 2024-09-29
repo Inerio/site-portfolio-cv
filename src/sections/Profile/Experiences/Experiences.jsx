a4735459abd6814361ff1d15dc52c615c6988e8b
Inerio <julien.dearaujo.pro@gmail.com>
2024-09-29 18:53:45 +0200

    Experiences

--- src/sections/Profile/Experiences/Experiences.jsx
+++ src/sections/Profile/Experiences/Experiences.jsx
@@ -1,13 +1,38 @@
 import styles from './ExperiencesStyles.module.css'
+import ExperienceCard from '../../../common/ExperienceCard'
+import siteLogo from '../../../assets/random-image.png'
+import merieuxLogo from '../../../assets/mérieux.png'
+import abasLogo from '../../../assets/abas-france-logo.png'
+import abbiStudioLogo from '../../../assets/abbi-studio-logo.png'
 
 function Experiences() {
     return <section id="projects" className={styles.container}>
         <h1 className="sectionTitle">Experiences</h1>
         <div className={styles.experiencesContainer}>
-            <a href="https://github.com/Inerio">
-
-            </a>
-
+            <ExperienceCard
+                src={siteLogo}
+                link='https://github.com/Inerio/site-portfolio-cv'
+                h3="Mon site"
+                p="Un portfolio en React"
+            />
+            <ExperienceCard
+                src={merieuxLogo}
+                link='https://github.com/Inerio/site-portfolio-cv'
+                h3="Mérieux Nutriscience"
+                p="Data developer / Full Stack"
+            />
+            <ExperienceCard
+                src={abasLogo}
+                link='https://github.com/Inerio/site-portfolio-cv'
+                h3="ABAS"
+                p="Java developer"
+            />
+            <ExperienceCard
+                src={abbiStudioLogo}
+                link='https://github.com/Inerio/site-portfolio-cv'
+                h3="Abbi Studio"
+                p="Angular developer"
+            />
         </div>
     </section>
 }
