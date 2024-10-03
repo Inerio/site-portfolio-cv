cf338d1990cc03ae5c3eef72de9553fe48d1fd2b
Inerio <julien.dearaujo.pro@gmail.com>
2024-10-03 16:38:41 +0200

    Hot fix to increase readability + responsive

--- src/assets/Sections/Experiences/ExperienceDetails.jsx
+++ src/assets/Sections/Experiences/ExperienceDetails.jsx
@@ -1,13 +1,14 @@
+import styles from './ExperienceDetailsStyles.module.css'
 import { useThemeIcons } from '../../Hooks/useThemeIcons';
 
 function ExperienceDetails({ experience, setToggleExperience }) {
     if (!experience) return null;
     const icons = useThemeIcons();
 
-    return (
-        <div>
-            <h2>{experience.title}</h2>
-            <p>{experience.description}</p>
+    return <section id={experience.title} className={styles.container}>
+        <h1 className="sectionTitle">{experience.title}</h1>
+        <div className={styles.experiencesContainer}>
+            <h2>{experience.description}</h2>
             <p>{experience.details}</p>
             <a
                 href={experience.link}
@@ -15,12 +16,12 @@ function ExperienceDetails({ experience, setToggleExperience }) {
                 Aller vers {experience.title}
             </a>
             <div onClick={() => setToggleExperience(false)}>
-                <img
+                <img className={styles.colorMode}
                     src={icons.back}
                     alt="Back icon" />
             </div>
         </div>
-    );
+    </section>
 }
 
 export default ExperienceDetails
--- src/assets/Sections/Experiences/Experiences.jsx
+++ src/assets/Sections/Experiences/Experiences.jsx
@@ -16,7 +16,11 @@ function Experiences({ setToggleExperience, setSelectedExperience }) {
                 p="Un portfolio en React"
                 setToggleExperience={setToggleExperience}
                 setSelectedExperience={setSelectedExperience}
-                details="Le site sur lequel vous êtes..."
+                details="Le site sur lequel vous êtes... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas feugiat consequat nisi in semper. 
+                Pellentesque sed turpis ac ipsum lacinia rutrum vestibulum hendrerit enim. Mauris diam erat, euismod sit amet aliquam non, malesuada ac velit. 
+                Duis ante lacus, tincidunt sed massa ut, pellentesque consequat nisl. Aliquam mattis dui vulputate mattis fringilla. Maecenas lacinia accumsan maximus. 
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam metus augue, blandit tempor metus at, mollis aliquet dolor. 
+                Aenean facilisis nulla accumsan laoreet blandit."
             />
             <ExperienceCard
                 src={merieuxLogo}
@@ -25,7 +29,11 @@ function Experiences({ setToggleExperience, setSelectedExperience }) {
                 p="Data developer / Full Stack"
                 setToggleExperience={setToggleExperience}
                 setSelectedExperience={setSelectedExperience}
-                details="Chez Mérieux Nutriscience, j'ai travaillé sur..."
+                details="Chez Mérieux Nutriscience, j'ai travaillé sur... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas feugiat consequat nisi in semper. 
+                Pellentesque sed turpis ac ipsum lacinia rutrum vestibulum hendrerit enim. Mauris diam erat, euismod sit amet aliquam non, malesuada ac velit. 
+                Duis ante lacus, tincidunt sed massa ut, pellentesque consequat nisl. Aliquam mattis dui vulputate mattis fringilla. Maecenas lacinia accumsan maximus. 
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam metus augue, blandit tempor metus at, mollis aliquet dolor. 
+                Aenean facilisis nulla accumsan laoreet blandit."
             />
             <ExperienceCard
                 src={abasLogo}
@@ -34,7 +42,11 @@ function Experiences({ setToggleExperience, setSelectedExperience }) {
                 p="Java developer"
                 setToggleExperience={setToggleExperience}
                 setSelectedExperience={setSelectedExperience}
-                details="Chez ABAS, j'ai travaillé sur..."
+                details="Chez ABAS, j'ai travaillé sur... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas feugiat consequat nisi in semper. 
+                Pellentesque sed turpis ac ipsum lacinia rutrum vestibulum hendrerit enim. Mauris diam erat, euismod sit amet aliquam non, malesuada ac velit. 
+                Duis ante lacus, tincidunt sed massa ut, pellentesque consequat nisl. Aliquam mattis dui vulputate mattis fringilla. Maecenas lacinia accumsan maximus. 
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam metus augue, blandit tempor metus at, mollis aliquet dolor. 
+                Aenean facilisis nulla accumsan laoreet blandit."
             />
             <ExperienceCard
                 src={abbiStudioLogo}
@@ -43,7 +55,11 @@ function Experiences({ setToggleExperience, setSelectedExperience }) {
                 p="Angular developer"
                 setToggleExperience={setToggleExperience}
                 setSelectedExperience={setSelectedExperience}
-                details="Chez Abbi Studio, j'ai travaillé sur..."
+                details="Chez Abbi Studio, j'ai travaillé sur... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas feugiat consequat nisi in semper. 
+                Pellentesque sed turpis ac ipsum lacinia rutrum vestibulum hendrerit enim. Mauris diam erat, euismod sit amet aliquam non, malesuada ac velit. 
+                Duis ante lacus, tincidunt sed massa ut, pellentesque consequat nisl. Aliquam mattis dui vulputate mattis fringilla. Maecenas lacinia accumsan maximus. 
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam metus augue, blandit tempor metus at, mollis aliquet dolor. 
+                Aenean facilisis nulla accumsan laoreet blandit."
             />
         </div>
     </section>
